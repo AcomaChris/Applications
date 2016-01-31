7612b176a6518ad47da481ca6447a302fc4df494
AcomaChris <Mara_Uk@hotmail.com>
2016-01-31 15:59:29 -0700

    Committing Toby's changes.

--- DiceRollingGame/DiceRollingGame/DiceRollingGame/Program.cs
+++ DiceRollingGame/DiceRollingGame/DiceRollingGame/Program.cs
@@ -10,7 +10,28 @@ namespace DiceRollingGame
     {
         static void Main(string[] args)
         {
-            Console.ReadLine();
+            String Player1Name;
+            String Player2Name;
+            int CurrentDiceRoll;
+            int Player1Total;
+            int Player2Total;
+            Random rnd = new Random();
+            int total = 0;
+
+            Console.WriteLine("Enter Player 1's Name: ");
+            Player1Name = Console.ReadLine();
+            Console.WriteLine("Greetings, " + Player1Name);
+
+            Console.WriteLine("Enter Player 2's Name: ");
+            Player2Name = Console.ReadLine();
+            Console.WriteLine("Greetings, " + Player2Name);
+
+            Console.WriteLine("Please enter an Integer to define the total score");
+            total = Console.Read();
+            Console.WriteLine("The target is " + total + ".");
+
+            //Stops the application from exiting immediately
+            Console.ReadKey();
         }
     }
 }
